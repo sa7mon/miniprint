6734585f26c61c27656b16a0195bd6224127a717
= <=>
2019-03-21 19:46:01 +0000

    add dev setup for code-server

---
+++ dev-setup.sh
@@ -0,0 +1,5 @@
+apt install python3 python3-pip python 
+pip install -r requirements.txt
+git config --global --add user.name "Dan Salmon"
+git config --global --add user.email "dan.sa7mon@gmail.com"
+git clone https://github.com/RUB-NDS/PRET.git
